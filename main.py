1d11d3a176633fff22f05ec3df59d93d863f9676
Joey Ma <jlsajfj@hotmail.com>
2022-11-19 14:51:20 -0500

    added url protocol

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import spin
 import requests
 
 
-alert_url = "100.126.117.114:5000/alert"
+alert_url = "http://100.126.117.114:5000/alert"
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
@@ -20,7 +20,7 @@ if __name__ == '__main__':
                 spin.open_door('left')
             elif int.from_bytes(number, byteorder='big') == 2:
                 print("Threshold reached for 2")
-                response = requests.request("GET", alert_url)
+                response = requests.request("GET", alert_url, headers={}, data={})
                 spin.open_door('right')
             
             ser.reset_input_buffer()
