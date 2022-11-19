c050d255a3582f52371f222ddf8c16d9762b2e1b
Joey Ma <jlsajfj@hotmail.com>
2022-11-19 11:51:28 -0500

    added helper

---
+++ spin.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+SLEEP_TIME = 0.01
+
+#assign GPIO pins for motor
+motor_channel = (29,31,33,35)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+#for defining more than 1 GPIO channel as input/output use
+GPIO.setup(motor_channel, GPIO.OUT)
+
+def cw(n):
+    for _ in range(n):
+        GPIO.output(motor_channel, (GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH))
+        sleep(SLEEP_TIME)
+        GPIO.output(motor_channel, (GPIO.HIGH,GPIO.HIGH,GPIO.LOW,GPIO.LOW))
+        sleep(SLEEP_TIME)
+        GPIO.output(motor_channel, (GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW))
+        sleep(SLEEP_TIME)
+        GPIO.output(motor_channel, (GPIO.LOW,GPIO.LOW,GPIO.HIGH,GPIO.HIGH))
+        sleep(SLEEP_TIME)
+
+def ccw(n):
+    for _ in range(n):
+        GPIO.output(motor_channel, (GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH))
+        sleep(SLEEP_TIME)
+        GPIO.output(motor_channel, (GPIO.LOW,GPIO.LOW,GPIO.HIGH,GPIO.HIGH))
+        sleep(SLEEP_TIME)
+        GPIO.output(motor_channel, (GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW))
+        sleep(SLEEP_TIME)
+        GPIO.output(motor_channel, (GPIO.HIGH,GPIO.HIGH,GPIO.LOW,GPIO.LOW))
+        sleep(SLEEP_TIME)
