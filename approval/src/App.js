f3f42e529098d079bf37d3ebc37e46aaf8116ed9
Joey Ma <jlsajfj@hotmail.com>
2022-11-19 21:37:32 -0500

    updating locations for buttons

--- approval/src/App.js
+++ approval/src/App.js
@@ -6,29 +6,35 @@ import Button from '@mui/material/Button';
 
 // maybe insert some logic that changes the text based on image displayed
 
+function Main(props) {
+  return (
+    <header className="App-header">
+      <img src={"http://100.126.117.114:5000/photo"} className="doggo" alt="doggo" />
+      <p>
+        A potential entry in your backyard has been detected.
+      </p>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" size="large"
+          onClick={() => {
+            fetch("http://100.126.117.114:5000/approve");
+          }}
+        >Approve
+        </Button>
+        <Button variant="outlined" size="large"
+          onClick={() => {
+            fetch("http://100.126.117.114:5000/deny");
+          }}>
+          Deny
+        </Button>
+      </Stack>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={"http://100.126.117.114:5000/photo"} className="doggo" alt="doggo" />
-        <p>
-          A potential entry in your backyard has been detected.
-        </p>
-        <Stack spacing={2} direction="row">
-          <Button variant="contained" size="large"
-            onClick={() => {
-              fetch("http://100.126.117.114:5000/approve");
-            }}
-          >Approve
-          </Button>
-          <Button variant="outlined" size="large"
-            onClick={() => {
-              fetch("http://100.126.117.114:5000/deny");;
-            }}>
-            Deny
-          </Button>
-        </Stack>
-      </header>
+      <Main />
     </div>
   );
 }
