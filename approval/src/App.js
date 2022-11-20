cd5fa0d1dc3b2d1f354ed740ee0605ba3de93363
Joey Ma <jlsajfj@hotmail.com>
2022-11-19 23:18:33 -0500

    created feedback on usage

--- approval/src/App.js
+++ approval/src/App.js
@@ -4,15 +4,16 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-import {toast} from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-toast.configure()
+//toast.configure()
 
 // maybe insert some logic that changes the text based on image displayed
 
 function BodyData(props) {
   return (
     <header className="App-header">
+      <ToastContainer autoClose={3000} />
       <img src={"http://100.126.117.114:5000/photo"} className="doggo" alt="doggo" />
       <p>
         A potential entry in your backyard has been detected.
@@ -20,26 +21,23 @@ function BodyData(props) {
       <Stack spacing={2} direction="row">
         <Button variant="contained" size="large"
           onClick={() => {
-            fetch("http://100.126.117.114:5000/approve");
-            //notify()
+            fetch("http://100.126.117.114:5000/approve").then(resp => resp.text()).then(data => {
+              toast(data)
+            });
           }}
         >Approve
         </Button>
         <Button variant="outlined" size="large"
           onClick={() => {
-            fetch("http://100.126.117.114:5000/deny");
-            //notify()
+            fetch("http://100.126.117.114:5000/deny").then(resp => resp.text()).then(data => {
+              toast(data)
+            });
           }}>
           Deny
         </Button>
       </Stack>
     </header>
   );
-}
-
-function notify() {
-  // Calling toast method by passing string
-  toast('potatos')
 }
 
 function App() {
